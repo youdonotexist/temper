7fde97b00b271dec7c53d5eaf4a5b0000ca3842f
- <me@basically.cam>
2020-11-11 14:03:25 -0800

    Updaing name of import

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask_restful import Api, Resource
 from apscheduler.schedulers.background import BackgroundScheduler
 
 import requests
-import main
+import tempsensor
 import time
 
 app = Flask(__name__)
 api = Api(app)
 
 def sendTemp():
-    temp = main.read_temp()
+    temp = tempsensor.read_temp()
     dictToSend = {'temp': temp[1], 'timestamp': time.time() }
     res = requests.post('http://192.168.0.11:5022/saveTemp', json=dictToSend)
     print('response from server:',res.text)
@@ -21,7 +21,7 @@ scheduler.start()
 
 class CurrentTemp(Resource):
     def get(self):
-        temp = main.read_temp()
+        temp = tempsensor.read_temp()
         return jsonify(c=temp[0],f=temp[1])
 
 api.add_resource(CurrentTemp, '/current')
